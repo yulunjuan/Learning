2be9ce1eddcdb6f0ba3f155b951b440b8c879b4a
yulunjuan <allenjuan.aj@gmail.com>
2023-03-17 19:23:19 +0800

    230317 補上傳

--- Cpp/DSA_textbook_practice/CH3_1_Array/main.cpp
+++ Cpp/DSA_textbook_practice/CH3_1_Array/main.cpp
@@ -2,48 +2,9 @@
 
 using namespace std;
 
-class GameEntry{
-public:
-    GameEntry(const string& n = "", int s = 0);
-    ~GameEntry();
-    string getName() const;
-    int getScore() const;
-private:
-    string name;
-    int score;
-};
-
-GameEntry::GameEntry(const string& n, int s):name(n),score(s){}
-string GameEntry::getName() const{return name;}
-int GameEntry::getScore() const{return score;}
-
-
-
-class Scores{
-public:
-    Scores(int maxEnt = 10);
-    ~Scores();
-    void add(const GameEntry& e);
-    GameEntry remove(int i);  // throw (IndexOutOfBounds);
-
-private:
-    int maxEntries;
-    int numEntries;
-    GameEntry* entries;
-};
-
-Scores::Scores (int maxEnt){
-    maxEntries = maxEnt;
-    entries = new GameEntry [maxEntries];
-    numEntries = 0;
-}
-
-Scores::~Scores() { delete [] entries;}
-
-
 int main() {
 
-
+    cout << __cplusplus << endl;
 
     return 0;
 }
